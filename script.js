5988ace14fcd79eabeb5bdf5aa462bbc941d3a28
mohamed <mohamedeng747@gmail.com>
2026-03-01 00:50:54 +0200

    m

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ document.querySelectorAll(".navbar ul li a").forEach((link) => {
 
 // بتاع الناف بار ينتهي
 
+
+
+/* About Start */
+
+// وظيفة لإظهار العناصر عند التمرير (Scroll)
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// تشغيلها مرة عند التحميل عشان لو السيكشن باين أصلاً
+reveal();
+
+/* About End */
